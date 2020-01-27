484d26573a2f94f1edad849ec085b523bc8e0fb2
Mike Miranda <mikemiranda1998@gmail.com>
2020-01-27 17:59:48 -0500

    update

--- Mobile Application (CSCE 546)/TestApp/ionicReactGuide/src/pages/Home.tsx
+++ Mobile Application (CSCE 546)/TestApp/ionicReactGuide/src/pages/Home.tsx
@@ -1,23 +1,57 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonItemOption,  IonAvatar, IonToolbar, IonList, IonItem, IonText, IonLabel, IonItemSliding, IonItemOptions } from '@ionic/react';
 import React from 'react';
+
+const arr = [
+  {
+    name: "Finn",
+    desc: 'Tried to kill Himself'
+  },
+  {
+    name: "Han",
+    desc: 'Has a Dumb Son'
+  },
+  {
+    name: "Rey",
+    desc: 'Who my parents'
+  },
+
+]
+
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+        <IonToolbar >
+          <IonTitle class="ion-text-center">Ionic Title in a Toolbar</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        Hello World
+        <IonList>
+          {arr.map((element,i) => (
+
+            <IonItemSliding>
+
+              <IonItem>
+                <IonAvatar>
+                    <img src={`https://ionicframework.com/docs/demos/api/list/avatar-${element.name.toLowerCase()}.png`} >
+                    </img>
+                </IonAvatar>  
+
+                <IonLabel>
+                  <h2>{element.name}</h2>
+                  <p>{element.desc}</p>
+                </IonLabel>
+               </IonItem>
+
+                <IonItemOptions side = "start">
+                  <IonItemOption onClick={() => alert('pressed Option')}> Hello </IonItemOption>
+                </IonItemOptions>
+
+          </IonItemSliding>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
